d6e7f14615d70c4b847f332896521a2cab58c875
Lance Langlois <lancelanglois05@gmail.com>
2026-08-08 23:57:45 -0400

    Migrate from Create React App to Vite

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="content-box">
+          <p>
+          Hello! <br />
+          My Name is Lance Langlois
+          </p>
+        </div>
+      </header>
+
+      <main className="Main-Content">
+        <div className="content-box">
+          <p>
+            About Me Box
+          </p>
+        </div>
+        
+        <div className="content-box">
+          <p>
+            technical expertise box
+          </p>
+        </div>
+
+        <div className="content-box">
+          <p>
+            interest box
+          </p>
+        </div>
+      </main>
+
+      <footer className="App-footer">
+          <p>
+            information
+          </p>
+      </footer>
+    
+    </div>
+  );
+}
+
+
+export default App; 
+
